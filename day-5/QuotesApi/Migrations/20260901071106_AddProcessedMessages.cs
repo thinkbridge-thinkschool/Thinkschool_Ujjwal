dbc1144031247c858b2ee2b66fa61bf080072f66
picxellence <ujjwalsrivastava1710@gmail.com>
2026-09-01 14:13:23 +0530

    Day 19: Service Bus publisher, dual subscription workers, composite dedupe key

---
+++ day-5/QuotesApi/Migrations/20260901071106_AddProcessedMessages.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuotesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessedMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProcessedMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SubscriptionName = table.Column<string>(type: "TEXT", nullable: false),
+                    MessageId = table.Column<string>(type: "TEXT", nullable: false),
+                    ProcessedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessedMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcessedMessages_SubscriptionName_MessageId",
+                table: "ProcessedMessages",
+                columns: new[] { "SubscriptionName", "MessageId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcessedMessages");
+        }
+    }
+}
